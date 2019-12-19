ef1c4e97d9dfd1edddd2a0f23e33696517d3d32a
Alex Soto <asotobu@gmail.com>
2019-12-19 19:43:21 +0100

    Fix

--- src/test/java/org/acme/ExampleResourceTest.java
+++ src/test/java/org/acme/ExampleResourceTest.java
@@ -15,7 +15,7 @@ public class ExampleResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hola"));
+             .body(is("hello"));
     }
 
 }
